d7b31c3d7653cfceb6860c308cda6042a07532c8
samuel-chai <ccs95209@gmail.com>
2024-11-13 14:47:11 +0800

    delete 3d drawing, edit yolo_realtime

--- tool/yolo_realtime.py
+++ tool/yolo_realtime.py
@@ -10,33 +10,36 @@ from tkinter import filedialog
 import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
-from video import Keypoint, getKptsFromImage
-from utilsCombine import videopose_model_load, interface, videoInfo, resize_img ,draw_3Dimg, draw_2Dimg
+from yolo_detectors.yolov8_detection import Keypoint
+from utils import videopose_model_load, interface, videoInfo, resize_img ,draw_3Dimg, draw_2Dimg
 
-# Load YOLO model for real-time pose estimation
-pose_model = Keypoint(model_path='weights/yolov8x-pose.onnx')
-interface2D = getKptsFromImage
-interface3D = interface
+model_path = 'weights/yolov8x-pose.onnx'
+keydet = Keypoint(model_path)
 model3D = videopose_model_load()
+
 
 
 def main(VideoName):
     cap, cap_length = videoInfo(VideoName)
     kpt2Ds = []
+    processed_frames = []
+
     for i in tqdm(range(cap_length)):
         _, frame = cap.read()
         frame, W, H = resize_img(frame)
 
         try:
             t0 = time.time()
-            # Using the YOLO model to extract 2D keypoints
-            bboxs, joint2D = interface2D(pose_model, frame)
+            joint2D = np.array(keydet.getkptsFromImg(frame))
             print('YOLO consume {:0.3f} s'.format(time.time() - t0))
-
+            # 檢查形狀, 若數據不對跳過frame
+            if joint2D.shape[-2:] != (17, 3):
+                print("Skipping frame due to incorrect shape:", joint2D.shape)
+                continue
         except Exception as e:
             print(e)
             continue
-
+        
         if i == 0:
             for _ in range(30):
                 kpt2Ds.append(joint2D)
@@ -45,11 +48,21 @@ def main(VideoName):
             kpt2Ds.pop(0)
         else:
             kpt2Ds.append(joint2D)
+                
+        joint3D = interface(model3D, np.array(kpt2Ds), W, H)
+        joint3D_item = joint3D[-1] # (17, 3)
+        processed_frame = draw_3Dimg(joint3D_item, frame, display=1, kpt2D=joint2D)
+        processed_frames.append(processed_frame)
+     # 在所有幀處理完成後，保存處理過的幀為視頻
+    if processed_frames:
+        height, width, layers = processed_frames[0].shape
+        out = cv2.VideoWriter('output.avi', cv2.VideoWriter_fourcc(*'XVID'), 20, (width, height))
 
-        joint3D = interface3D(model3D, np.array(kpt2Ds), W, H)
-        joint3D_item = joint3D[-1] #(17, 3)
-        draw_3Dimg(joint3D_item, frame, display=1, kpt2D=joint2D)
-
+        for frame in processed_frames:
+            out.write(frame)
+        out.release()
+        print("Processed video saved as 'output.avi'.")
+            
 if __name__ == '__main__':
     # 初始化 Tkinter，隐藏主窗口
     root = tk.Tk()
@@ -67,4 +80,5 @@ if __name__ == '__main__':
     else:
         print('Input Video Name is ', VideoName)
         main(VideoName)
+
 
--- yolo_detectors/yolov8_detection.py
+++ yolo_detectors/yolov8_detection.py
@@ -10,22 +10,37 @@ from tkinter import filedialog
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
 
-# 定义一个调色板数组，其中每个元素是一个包含RGB值的列表，用于表示不同的颜色
-palette = np.array([[255, 128, 0], [255, 153, 51], [255, 178, 102],
-                    [230, 230, 0], [255, 153, 255], [153, 204, 255],
-                    [255, 102, 255], [255, 51, 255], [102, 178, 255],
-                    [51, 153, 255], [255, 153, 153], [255, 102, 102],
-                    [255, 51, 51], [153, 255, 153], [102, 255, 102],
-                    [51, 255, 51], [0, 255, 0], [0, 0, 255], [255, 0, 0],
-                    [255, 255, 255]])
-# 定义人体17个关键点的连接顺序，每个子列表包含两个数字，代表要连接的关键点的索引, 1鼻子 2左眼 3右眼 4左耳 5右耳 6左肩 7右肩 8左肘 9右肘 10左手腕 11右手腕 12左髋 13右髋 14左膝 15右膝 16左踝 17右踝
-skeleton = [[16, 14], [14, 12], [17, 15], [15, 13], [12, 13], [6, 12],
-            [7, 13], [6, 7], [6, 8], [7, 9], [8, 10], [9, 11], [2, 3],
-            [1, 2], [1, 3], [2, 4], [3, 5], [4, 6], [5, 7]]
-# 通过索引从调色板中选择颜色，用于绘制人体骨架的线条，每个索引对应一种颜色
-pose_limb_color = palette[[9, 9, 9, 9, 7, 7, 7, 0, 0, 0, 0, 0, 16, 16, 16, 16, 16, 16, 16]]
-# 通过索引从调色板中选择颜色，用于绘制人体的关键点，每个索引对应一种颜色
-pose_kpt_color = palette[[16, 16, 16, 16, 16, 0, 0, 0, 0, 0, 0, 9, 9, 9, 9, 9, 9]]
+class common():
+    # 定义一个调色板数组，其中每个元素是一个包含RGB值的列表，用于表示不同的颜色
+    palette = np.array([[255, 128, 0], [255, 153, 51], [255, 178, 102],
+                        [230, 230, 0], [255, 153, 255], [153, 204, 255],
+                        [255, 102, 255], [255, 51, 255], [102, 178, 255],
+                        [51, 153, 255], [255, 153, 153], [255, 102, 102],
+                        [255, 51, 51], [153, 255, 153], [102, 255, 102],
+                        [51, 255, 51], [0, 255, 0], [0, 0, 255], [255, 0, 0],
+                        [255, 255, 255]])
+    skeleton_parents =  np.array([-1,  0,  1,  2,  0,  4,  5,  0,  7,  8,  9,  8, 11, 12,  8, 14, 15])
+
+    # 定义人体17个关键点的连接顺序，每个子列表包含两个数字，代表要连接的关键点的索引, 1鼻子 2左眼 3右眼 4左耳 5右耳 6左肩 7右肩 8左肘 9右肘 10左手腕 11右手腕 12左髋 13右髋 14左膝 15右膝 16左踝 17右踝
+    skeleton = [[0, 1], [1, 3], [0, 2], [2, 4],
+                [5, 6], [5, 7], [7, 9], [6, 8], [8, 10],
+                [5, 11], [6, 12], [11, 12],
+                [11, 13], [12, 14], [13, 15], [14, 16]]
+    # 通过索引从调色板中选择颜色，用于绘制人体骨架的线条，每个索引对应一种颜色
+    pose_limb_color = palette[[9, 9, 9, 9, 7, 7, 7, 0, 0, 0, 0, 0, 16, 16, 16, 16, 16, 16, 16]]
+    # 通过索引从调色板中选择颜色，用于绘制人体的关键点，每个索引对应一种颜色
+    pose_kpt_color = palette[[16, 16, 16, 16, 16, 0, 0, 0, 0, 0, 0, 9, 9, 9, 9, 9, 9]]
+    pad = (243 - 1) // 2 # Padding on each side
+    causal_shift = 0
+    
+    # 对称关节的定义（左、右）
+    kps_left = [1, 3, 5, 7, 9, 11, 13, 15]  # 左肩、左肘、左手腕、左髋、左膝、左踝
+    kps_right = [2, 4, 6, 8, 10, 12, 14, 16]  # 右肩、右肘、右手腕、右髋、右膝、右踝
+    
+    # 定义左关节和右关节（这些是更详细的关节连接用于对称处理）
+    joints_left = list([4, 5, 6, 11, 12, 13])  # 左肩、左肘、左手腕、左髋、左膝、左踝
+    joints_right = list([1, 2, 3, 14, 15, 16])  # 右肩、右肘、右手腕、右髋、右膝、右踝
+    rot = np.array([ 0.14070565, -0.15007018, -0.7552408 ,  0.62232804], dtype=np.float32)
 
 providers = [
     ('CUDAExecutionProvider', {
@@ -39,39 +54,26 @@ def letterbox(im, new_shape=(640, 640), color=(114, 114, 114), scaleup=True):
     shape = im.shape[:2]
     if isinstance(new_shape, int):
         new_shape = (new_shape, new_shape)
-    # 缩放比例 (new / old)
     r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
-    # 只进行下采样 因为上采样会让图片模糊
     if not scaleup:
         r = min(r, 1.0)
     # 计算pad长宽
     new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))  # 保证缩放后图像比例不变
     dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]  # wh padding
-    # 在较小边的两侧进行pad, 而不是在一侧pad
     dw /= 2
     dh /= 2
-    # 将原图resize到new_unpad（长边相同，比例相同的新图）
     if shape[::-1] != new_unpad:  # resize
         im = cv2.resize(im, new_unpad, interpolation=cv2.INTER_LINEAR)
-    # 计算上下两侧的padding
     top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
-    # 计算左右两侧的padding
     left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
-    # 添加灰条
     im = cv2.copyMakeBorder(im, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)
     return im
 
 def pre_process(img):
-    # 归一化：将图像数据从0~255缩放到0~1之间，这一步是为了让模型更容易学习。
     img = img / 255.
-    # 调整通道顺序：将图像从高度x宽度x通道数（H, W, C）调整为通道数x高度x宽度（C, H, W）的形式。
-    # 这样做是因为许多深度学习框架要求输入的通道数在前。
     img = np.transpose(img, (2, 0, 1))
-    # 增加一个维度：在0轴（即最前面）增加一个维度，将图像的形状从（C, H, W）变为（1, C, H, W）。
-    # 这一步是为了满足深度学习模型输入时需要的批量大小（batch size）的维度，即使这里的批量大小为1。
     data = np.expand_dims(img, axis=0)
     return data
-
 
 def xywh2xyxy(x):
     ''' 中心坐标、w、h ------>>> 左上点，右下点 '''
@@ -83,7 +85,6 @@ def xywh2xyxy(x):
     return y
 
 def nms(dets, iou_thresh):
-    # dets: N * M, N是bbox的个数，M的前4位是对应的 左上点，右下点
     x1 = dets[:, 0]
     y1 = dets[:, 1]
     x2 = dets[:, 2]
@@ -95,20 +96,15 @@ def nms(dets, iou_thresh):
     while order.size > 0:
         i = order[0]  # 无条件保留每次迭代中置信度最高的bbox
         keep.append(i)
-        # 计算置信度最高的bbox和其他剩下bbox之间的交叉区域
         xx1 = np.maximum(x1[i], x1[order[1:]])
         yy1 = np.maximum(y1[i], y1[order[1:]])
         xx2 = np.minimum(x2[i], x2[order[1:]])
         yy2 = np.minimum(y2[i], y2[order[1:]])
-        # 计算置信度高的bbox和其他剩下bbox之间交叉区域的面积
         w = np.maximum(0.0, xx2 - xx1 + 1)
         h = np.maximum(0.0, yy2 - yy1 + 1)
         inter = w * h
-        # 求交叉区域的面积占两者（置信度高的bbox和其他bbox）面积和的必烈
         ovr = inter / (areas[i] + areas[order[1:]] - inter)
-        # 保留ovr小于thresh的bbox，进入下一次迭代。
         inds = np.where(ovr <= iou_thresh)[0]
-        # 因为ovr中的索引不包括order[0]所以要向后移动一位
         order = order[inds + 1]
     output = []
     for i in keep:
@@ -116,20 +112,13 @@ def nms(dets, iou_thresh):
     return np.array(output)
 
 def xyxy2xywh(a):
-    ''' 左上点 右下点 ------>>> 左上点 宽 高 '''
+
     b = np.copy(a)
     b[:, 2] = a[:, 2] - a[:, 0]  # w
     b[:, 3] = a[:, 3] - a[:, 1]  # h
     return b
 
 def scale_boxes(img1_shape, boxes, img0_shape):
-    '''   将预测的坐标信息转换回原图尺度
-    :param img1_shape: 缩放后的图像尺度
-    :param boxes:  预测的box信息
-    :param img0_shape: 原始图像尺度
-    '''
-
-    # 将检测框(x y w h)从img1_shape(预测图) 缩放到 img0_shape(原图)
     gain = min(img1_shape[0] / img0_shape[0], img1_shape[1] / img0_shape[1])  # gain  = old / new
     pad = (img1_shape[1] - img0_shape[1] * gain) / 2, (img1_shape[0] - img0_shape[0] * gain) / 2  # wh padding
     boxes[:, 0] -= pad[0]
@@ -144,8 +133,6 @@ def scale_boxes(img1_shape, boxes, img0_shape):
     return boxes
 
 def clip_boxes(boxes, shape):
-    # 进行一个边界截断，以免溢出
-    # 并且将检测框的坐标（左上角x，左上角y，宽度，高度）--->>>（左上角x，左上角y，右下角x，右下角y）
     top_left_x = boxes[:, 0].clip(0, shape[1])
     top_left_y = boxes[:, 1].clip(0, shape[0])
     bottom_right_x = (boxes[:, 0] + boxes[:, 2]).clip(0, shape[1])
@@ -155,18 +142,18 @@ def clip_boxes(boxes, shape):
     boxes[:, 2] = bottom_right_x  # 右下
     boxes[:, 3] = bottom_right_y
 
-def plot_skeleton_kpts(im, kpts, steps=3):
+def plot_skeleton_kpts(im, kpts, pose_kpt_color, pose_limb_color, skeleton, steps=3):
     num_kpts = len(kpts) // steps  # 51 / 3 =17
     # 画点
     for kid in range(num_kpts):
-        r, g, b = pose_kpt_color[kid]
+        r, g, b = common.pose_kpt_color[kid]
         x_coord, y_coord = kpts[steps * kid], kpts[steps * kid + 1]
         conf = kpts[steps * kid + 2]
         if conf > 0.5:  # 关键点的置信度必须大于 0.5
             cv2.circle(im, (int(x_coord), int(y_coord)), 5, (int(r), int(g), int(b)), -1)
     # 画骨架
-    for sk_id, sk in enumerate(skeleton):
-        r, g, b = pose_limb_color[sk_id]
+    for sk_id, sk in enumerate(common.skeleton):
+        r, g, b = common.pose_limb_color[sk_id]
         pos1 = (int(kpts[(sk[0] - 1) * steps]), int(kpts[(sk[0] - 1) * steps + 1]))
         pos2 = (int(kpts[(sk[1] - 1) * steps]), int(kpts[(sk[1] - 1) * steps + 1]))
         conf1 = kpts[(sk[0] - 1) * steps + 2]
@@ -193,88 +180,55 @@ class Keypoint():
         pred = pred[pred[:, 4] > conf]
         if len(pred) == 0:
             print("没有检测到任何关键点")
-            return image
+            return None
         else:
-            # 中心宽高转左上点，右下点
             bboxs = xywh2xyxy(pred)
-            # NMS处理
             bboxs = nms(bboxs, iou_thresh=0.6)
-            # 坐标从左上点，右下点 到 左上点，宽，高.
             bboxs = np.array(bboxs)
             bboxs = xyxy2xywh(bboxs)
-            # 坐标点还原到原图
             bboxs = scale_boxes(img.shape, bboxs, image.shape)
-            # 画框 画点 画骨架
             for box in bboxs:
                 det_bbox, det_scores, kpts = box[0:4], box[4], box[5:]
                 label = "Person {:.2f}".format(det_scores)
                 (label_width, label_height), _ = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
-                # 画点 连线
-                plot_skeleton_kpts(image, kpts)
+                plot_skeleton_kpts(image, kpts, common.pose_kpt_color, common.pose_limb_color, common.skeleton)
+
             return image
+    def getkptsFromImg(self, image):
+        img = letterbox(image)
+        data = pre_process(img)
+        # 预测输出float32[1, 56, 8400]
+        pred = self.session.run([self.label_name], {self.input_name: data.astype(np.float32)})[0]
+        # [56, 8400]
+        pred = pred[0]
+        # [8400,56]
+        pred = np.transpose(pred, (1, 0))
+        # 置信度阈值过滤
+        conf = 0.7
+        pred = pred[pred[:, 4] > conf]
+        if len(pred) == 0:
+            print("没有检测到任何关键点")
+            return None
+        else:
+            bboxs = xywh2xyxy(pred)
+            bboxs = nms(bboxs, iou_thresh=0.6)
+            bboxs = np.array(bboxs)
+            bboxs = xyxy2xywh(bboxs)
+            bboxs = scale_boxes(img.shape, bboxs, image.shape)
+            keypoints_list = []
+            for box in bboxs:
+                kpts = box[5:]  # 获取关键点信息
+                keypoints = []
+                for i in range(0, len(kpts), 3):
+                    x, y, c = kpts[i], kpts[i+1], kpts[i+2]
+                    keypoints.append([x, y, c])
+                keypoints_list.append(keypoints)
+            if keypoints_list:
+                return np.array(keypoints_list)  # 应该返回形状为 (N, 17, 3) 的数组
+            return None
             
 if __name__ == '__main__':
-    model_path = 'weights/yolov8x-pose.onnx'
-    # 实例化模型
-    keydet = Keypoint(model_path)
-
-    # 创建一个隐藏的主窗口
-    root = tk.Tk()
-    root.withdraw()
-
-    # 弹出文件选择对话框选择输入视频文件
-    input_video_path = filedialog.askopenfilename(title="选择视频文件", filetypes=[("Video files", "*.mp4;*.avi;*.mov;*.mkv")])
-    if not input_video_path:
-        print("Error: No video file selected.")
-        exit()
-
-
-    # 打开视频文件
-    cap = cv2.VideoCapture(input_video_path)
-    # 检查视频是否成功打开
-    if not cap.isOpened():
-        print("Error: Could not open video.")
-        exit()
-    # 读取视频的基本信息
-    frame_width = int(cap.get(3))
-    frame_height = int(cap.get(4))
-    fps = cap.get(cv2.CAP_PROP_FPS)
-    # 定义视频编码器和创建VideoWriter对象
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # 根据文件名后缀使用合适的编码器
-
-    # 初始化帧数计数器和起始时间
-    frame_count = 0
-    start_time = time.time()
-
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            print("Info: End of video file.")
-            break
-
-        # 对读入的帧进行对象检测
-        output_image = keydet.inference(frame)
-
-        # 计算并打印帧速率
-        frame_count += 1
-        end_time = time.time()
-        elapsed_time = end_time - start_time
-        if elapsed_time > 0:
-            fps = frame_count / elapsed_time
-            print(f"FPS: {fps:.2f}")
-
-
-
-        # （可选）实时显示处理后的视频帧
-        cv2.imshow("Output Video", output_image)
-        key = cv2.waitKey(1)
-        if key == ord('q'):
-            break
-        elif key == ord(' '):  # Space to pause
-            cv2.waitKey(0)
-    # 释放资源
-    cap.release()
-    cv2.destroyAllWindows()
+    main()
 
 
 
